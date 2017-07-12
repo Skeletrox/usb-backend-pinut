5ecaad7ded05c9448d2c6b4f5ab147ea6e4ef823
Skeletrox <android.svr@gmail.com>
2017-07-12 17:21:56 +0530

    fix behavior for root

--- file-upload/fileupload/views.py
+++ file-upload/fileupload/views.py
@@ -175,7 +175,9 @@ def verify_USB(request):
 def download_to_USB(request):
     usb_name = get_usb_name()
     if usb_name is not None:
-        local_files_dir = '/home/' + getpass.getuser() + '/FILES/'
+        local_files_dir = getpass.getuser() + '/FILES/'
+        if os.geteuid != 0:
+            local_files_dir = '/home/' + getpass.getuser() + '/FILES/'
         local_files = []
         for root, folders, files in os.walk(local_files_dir):
             for file in files:
