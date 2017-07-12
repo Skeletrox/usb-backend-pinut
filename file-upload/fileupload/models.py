3eff4835d7d85b32eb7a485ae802dbd3c7dcec16
Skeletrox <android.svr@gmail.com>
2017-07-12 16:16:50 +0530

    usb download and extracted file deletion

--- file-upload/fileupload/models.py
+++ file-upload/fileupload/models.py
@@ -1,51 +1,42 @@
-
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .extract import extractit
-import os
 
-def get_extension(name):
-    length = len(name)
-    print 'LOOKIN FOR ' + name
-    try:
-        i = name.index('.')
-        return name[i+1:length]
-    except ValueError:
-        return 'N/A'
-
+# Create your models here.
 
 class EkFile(models.Model):
     file = models.FileField(upload_to="")
     slug = models.SlugField(max_length=50, blank=True)
-    link = models.CharField(max_length=500, default='NULL', blank=True)
-    file_type = models.CharField(max_length=10, default='N/A', blank=True)
+    type_of_file=models.CharField(max_length=50,blank=True)
+    path_of_file=models.CharField(max_length=250,blank=True)
 
     def __str__(self):
-        return self.slug
+        return self.file.name+","+self.type_of_file+","+self.path_of_file
 
-    @models.permalink
-    def get_absolute_url(self):
-        return ('upload-new', )
-
+   
     def save(self, *args, **kwargs):
         self.slug = self.file.name
-        self.link = '/var/www/ekstep/' + self.slug
-        self.file_type = get_extension(self.slug)
-        print 'EXTENSION IS ' + self.file_type
-        print 'SAVED AS ' + self.slug
-	print 'HAVING LINK ' + self.link
-        #print 'ALSO KNOWN AS ' + self.file
+        index=self.slug.rfind('.')
+        self.type_of_file=self.slug[index+1:]
+        self.path_of_file=self.file.path
+        print (self.path_of_file)
         super(EkFile, self).save(*args, **kwargs)
-
 
     def delete(self, *args, **kwargs):
         """delete -- Remove to leave file."""
-        link_name = os.getcwd() + 'file-upload/media/' + self.slug
-        #FileMetaData.objects.get(link=link_name).delete()
         self.file.delete(False)
         super(EkFile, self).delete(*args, **kwargs)
-
+        
+        
+class Content(models.Model):
+	ekfile=models.ForeignKey(EkFile,on_delete=models.CASCADE)
+	folder_file=models.CharField(max_length=250)
+	json_file=models.CharField(max_length=250)
+	def __str__(self):
+		return self.json_file+","+self.folder_file
+		
+	def save(self,*args,**kwargs):
+		super(Content,self).save(*args,**kwargs)
+		
+	def delete(self,*args,**kwargs):
+		super(Content,self).delete(*args,**kwargs)
 
 
