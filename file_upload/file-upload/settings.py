3f98f121342f77a1f00d517cc413cec98ef82057
Skeletrox <android.svr@gmail.com>
2017-07-22 14:24:47 +0530

    slight cleanup

--- file_upload/file-upload/settings.py
+++ file_upload/file-upload/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Oslo'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- file_upload/fileupload/static/js/main.js
+++ file_upload/fileupload/static/js/main.js
@@ -1,6 +1,19 @@
-$(function () {
-    'use strict';
+/*
+ * jQuery File Upload Plugin JS Example 8.8.2
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
 
+/*jslint nomen: true, regexp: true */
+/*global $, window, blueimp */
+
+$(function() {
+    'use strict';
     // Initialize the jQuery File Upload widget:
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
@@ -27,8 +40,8 @@ $(function () {
             // send Blob objects via XHR requests:
             disableImageResize: /Android(?!.*Chrome)|Opera/
                 .test(window.navigator.userAgent),
-            maxFileSize: 5000000000,
-            acceptFileTypes: /(\.|\/)(ecar)$/i
+            maxFileSize: 5000000,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
         });
         // Upload server status check for browsers with CORS support:
         if ($.support.cors) {
